5e56e15e3d5c310e2f44b8239cb46c86c3c13fbf
Arafin Ahmed <arafin2021@gmail.com>
2022-03-08 23:13:26 +0600

    Project setup

--- src/PropertyRenting/PropertyRenting.Web/Program.cs
+++ src/PropertyRenting/PropertyRenting.Web/Program.cs
@@ -1,11 +1,11 @@
-using Autofac.Extensions.DependencyInjection;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
-using Serilog;
-using Serilog.Events;
+using Microsoft.Extensions.Logging;
 using System;
-
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace PropertyRenting.Web
 {
@@ -13,40 +13,14 @@ namespace PropertyRenting.Web
     {
         public static void Main(string[] args)
         {
-            var configBuilder = new ConfigurationBuilder().AddJsonFile("appsettings.json", false)
-                .AddEnvironmentVariables()
-                .Build();
-
-            Log.Logger = new LoggerConfiguration()
-                .MinimumLevel.Debug()
-                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-                .Enrich.FromLogContext()
-                .ReadFrom.Configuration(configBuilder)
-                .CreateLogger();
-
-            try
-            {
-                Log.Information("Application Starting up");
-                CreateHostBuilder(args).Build().Run();
-            }
-            catch (Exception ex)
-            {
-                Log.Fatal(ex, "Application start-up failed");
-            }
-            finally
-            {
-                Log.CloseAndFlush();
-            }
+            CreateHostBuilder(args).Build().Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
-                .UseSerilog()
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
-                    webBuilder.UseUrls("http://*:80");
                 });
     }
 }
