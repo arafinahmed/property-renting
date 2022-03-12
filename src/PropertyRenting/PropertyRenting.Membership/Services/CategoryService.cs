41c1cd58624c872a7140356915e655d98ca25e60
Arafin Ahmed <arafin2021@gmail.com>
2022-03-12 19:29:46 +0600

    Category Service

--- src/PropertyRenting/PropertyRenting.Membership/Services/CategoryService.cs
+++ src/PropertyRenting/PropertyRenting.Membership/Services/CategoryService.cs
@@ -19,5 +19,40 @@ namespace PropertyRenting.Membership.Services
             _unitOfWork = unitOfWork;
             _profileService = profileService;
         }
+
+        public async Task CreateCategory(Category category, Guid modId)
+        {
+            if (category == null)
+                throw new ArgumentNullException("Category can not be null");
+
+            if (modId == Guid.Empty)
+                throw new ArgumentNullException("Moderator id can not be empty.");
+
+            var user = await _profileService.GetUserByIdAsync(modId);
+
+            if (user == null)
+                throw new FileNotFoundException("User not found with the modId");
+
+            var claims = await _profileService.GetClaimAsync(user);
+
+            if (claims == null)
+                throw new NullReferenceException("Claim is required for creating a Category");
+
+            var claim = claims.FirstOrDefault();
+
+            if (claim.Type != "Moderator")
+            {
+                throw new InvalidOperationException("You are not permited to create a Category");
+            }
+
+            var Categorys = _unitOfWork.Category.Get(x => x.CategoryName == category.CategoryName, "");
+
+            if (Categorys.Count > 0)
+                throw new InvalidOperationException("Category Name Already exists.");
+
+            await _unitOfWork.Category.AddAsync(new EO.Category { CategoryName = category.CategoryName });
+            _unitOfWork.Save();
+        }
+
     }
 }
