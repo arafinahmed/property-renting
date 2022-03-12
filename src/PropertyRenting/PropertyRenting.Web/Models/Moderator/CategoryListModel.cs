7b33d80befa6b53c4973bbb216e67cc51e2f9013
Arafin Ahmed <arafin2021@gmail.com>
2022-03-12 20:07:03 +0600

    Category list model

---
+++ src/PropertyRenting/PropertyRenting.Web/Models/Moderator/CategoryListModel.cs
@@ -0,0 +1,30 @@
+﻿using Autofac;
+using PropertyRenting.Membership.BusinessObject;
+using PropertyRenting.Membership.Services;
+
+namespace PropertyRenting.Web.Models.Moderator
+{
+    public class CategoryListModel
+    {
+        public IList<Category> Categories { get; set; }
+        private ILifetimeScope _scope;
+        private ICategoryService _categoryService;
+        public CategoryListModel() { }
+
+        public CategoryListModel(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        public void Resolve(ILifetimeScope scope)
+        {
+            _scope = scope;
+            _categoryService = _scope.Resolve<ICategoryService>();
+        }
+
+        public void LoadAllCategories()
+        {
+            //Categories = _categoryService.GetAllCategories();
+        }
+    }
+}
